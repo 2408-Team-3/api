f4e7db6349055af1102e328eb8b3d6f47a989c2d
Rebecca Biancofiore <rebeccabiancofiore79@gmail.comm>
2024-10-23 10:28:36 -0400

    schema sql file

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE error_logs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    line_number INT,
+    col_number INT,
+    project_id INT,
+    stack_trace TEXT,
+    handled BOOLEAN NOT NULL
+);
